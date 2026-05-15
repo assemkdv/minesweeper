d1200445a307ac3b66fcfb21316a19bca070f752
asem <102917982+assemkdv@users.noreply.github.com>
2026-05-15 14:22:56 +0500

    remove basePath and static export for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/minesweeper' : '',
-  trailingSlash: true,
   images: { unoptimized: true },
 };
 export default nextConfig;
